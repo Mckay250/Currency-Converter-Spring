1c20db7321207a2b7ab4ec72c26b8fd748d00f05
KayodeOke <kayode.oke@wizerconsulting.com>
2021-10-04 18:53:29 +0100

    Impl currency converter service

---
+++ src/main/java/com/nosto/currencyconverter/utils/Messages.java
@@ -0,0 +1,10 @@
+package com.nosto.currencyconverter.utils;
+
+public class Messages {
+    public static final int ERROR = 96;
+    public static final int SUCCESS = 0;
+    public static final int INFO = 25;
+    public static final String GENERAL_SUCCESS_MESSAGE = "Successful";
+    public static final String ZERO_OR_NEGATIVE_AMOUNT = "Amount cannot be equal to or less than zero";
+    public static final String GENERAL_ERROR_MESSAGE = "An error occurred from our end while processing your request. We will fix this ASAP. Sorry for the inconvenience";
+}
